7fb9be12557613cf4b2c53a6165b4e72fabd8e18
Ch Huzaifa <huzaifamansoor044@gmail.com>
2023-09-01 22:47:36 -0700

    Its an android Home Page

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "assignmentno1c"
+include(":app")
+ 
